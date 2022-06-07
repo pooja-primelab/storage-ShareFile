a2334c089560fba146fbec4eb54bd956047ee12e
Pooja Shah <shah.developer9@gmail.com>
2022-06-07 14:02:01 -0700

    Create Peer

--- src/main/main.go
+++ src/main/main.go
@@ -7,14 +7,39 @@ package main
 import (
 	"FileShare/src/fileshare"
 	"encoding/json"
-	"github.com/gorilla/mux"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func pingFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Pong")
+}
+
+func createPeer(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: CreatePeer")
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter id in order to create new Peer")
+	}
+
+	resBytes := []byte(reqBody)        // Converting the string "res" into byte array
+	var jsonRes map[string]interface{} // declaring a map for key names as string and values as interface
+	_ = json.Unmarshal(resBytes, &jsonRes)
+
+	var id int = int(jsonRes["id"].(float64))
+	fmt.Println("id", id)
+	testDirectory := "testdirs/peer" + strconv.Itoa(id) + "/"
+	port := ":" + strconv.Itoa(60120+id)
+	p1 := fileshare.MakePeer(id, testDirectory, port)
+
 	w.Header().Set("Contetnt-Type", "application/json")
-	json.NewEncoder(w).Encode(`Pong`)
+	json.NewEncoder(w).Encode(p1)
 }
 
 func main() {
@@ -33,6 +58,7 @@ func main() {
 		writer.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(writer).Encode(nodes)
 	})
+	router.HandleFunc("/createPeer", createPeer).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
